68c8fed8f4e86367f0d8001fd0fe93efcff6f60c
Jordy Wladimir Ortiz Aquino <jordanopro42@gmail.com>
2026-02-26 21:12:08 -0500

    Enhance object manipulation examples and improve console logging in script.js

--- HTML y JavaScript/1.-/script.js
+++ HTML y JavaScript/1.-/script.js
@@ -20,6 +20,30 @@ const constante = 'Esto es una constante';
 // constante = 'Cambio!!!'; //Esto genera un error
 
 // Si queremos imprimir el tipo de una variable bastaria con usar un console.log(typeof [Variable])
-console.log(typeof variableString)
-console.log(typeof variableInt)
-console.log(typeof variableBool)
+console.log(typeof variableString);
+console.log(typeof variableInt);
+console.log(typeof variableBool);
+
+// Para crear un objeto (diccionario) lo declaramos dentro de una variable 
+let Objeto = {
+    caracteristicas: 'Caracteristica A',
+    nombre: 'nombredelobjeto',
+};
+
+console.log(Objeto);
+
+// Para poder interactuar con los datos dentro de un objeto podrian usarse los siguientes comandos 
+
+// Impresión
+console.log(Objeto.nombre);
+console.log(Objeto['caracteristicas']);
+
+// Cambio de dato
+Objeto.nombre = 'otroNombre';
+Objeto['caracteristicas'] = 'Caracteristica B';
+
+console.log(Objeto)
+
+// Para poder borrar un dato dentro del diccionario se utiliza delete [Dato]
+delete Objeto.nombre;
+console.log(Objeto)
