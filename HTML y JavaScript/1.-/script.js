5f2ed022e73e64b4102ae6d83399af58682291d2
Jordy Wladimir Ortiz Aquino <jordanopro42@gmail.com>
2026-02-26 20:36:48 -0500

    Remove unused variable and adjust console logging examples in script.js

--- HTML y JavaScript/1.-/script.js
+++ HTML y JavaScript/1.-/script.js
@@ -4,7 +4,6 @@ console.log('Hola Mundo!');
 // Para la declaracion utilizamos el comando let [Nombre de variable] = [Dato];
 let variableString = 'Hola Mundo!';
 let variableInt = 10;
-let variableFloat = 2.5;
 let variableBool = true;
 let variableNull = null;
 let variableVacia;
@@ -12,7 +11,6 @@ let variableVacia;
 // Para el cambio de dato de una variable solo basta con invocarla y cambiar su valor
 variableString = 'Adios Mundo!';
 variableInt = 20;
-variableFloat = 3.5;
 variableBool = false;
 variableNull = 'Ahora tiene algo XD';
 variableVacia = 'Ahora tiene algo tambien XD';
@@ -20,3 +18,8 @@ variableVacia = 'Ahora tiene algo tambien XD';
 // Si queremos realizar una creacion de una variable con un dato constante, utilizaremos const [nombre de la constante] = [Dato];
 const constante = 'Esto es una constante';
 // constante = 'Cambio!!!'; //Esto genera un error
+
+// Si queremos imprimir el tipo de una variable bastaria con usar un console.log(typeof [Variable])
+console.log(typeof variableString)
+console.log(typeof variableInt)
+console.log(typeof variableBool)
