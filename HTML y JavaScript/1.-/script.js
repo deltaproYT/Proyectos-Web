0c9878dde356e9e05dddd36b376dc6f5084c97d2
Jordy Wladimir Ortiz Aquino <jordanopro42@gmail.com>
2026-02-26 20:29:41 -0500

    Add initial HTML and JavaScript files for basic structure and console logging

---
+++ HTML y JavaScript/1.-/script.js
@@ -0,0 +1,22 @@
+// Para escribir en la consola usamos el comando console.log;
+console.log('Hola Mundo!');
+
+// Para la declaracion utilizamos el comando let [Nombre de variable] = [Dato];
+let variableString = 'Hola Mundo!';
+let variableInt = 10;
+let variableFloat = 2.5;
+let variableBool = true;
+let variableNull = null;
+let variableVacia;
+
+// Para el cambio de dato de una variable solo basta con invocarla y cambiar su valor
+variableString = 'Adios Mundo!';
+variableInt = 20;
+variableFloat = 3.5;
+variableBool = false;
+variableNull = 'Ahora tiene algo XD';
+variableVacia = 'Ahora tiene algo tambien XD';
+
+// Si queremos realizar una creacion de una variable con un dato constante, utilizaremos const [nombre de la constante] = [Dato];
+const constante = 'Esto es una constante';
+// constante = 'Cambio!!!'; //Esto genera un error
