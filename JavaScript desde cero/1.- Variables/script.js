c72f1fef999c65403f135308344cc8e9ed51bcf9
Jordy Wladimir Ortiz Aquino <jordanopro42@gmail.com>
2026-03-01 04:12:10 -0500

    Remove outdated HTML and JavaScript files; add new structure and initial content for variables and operators sections

---
+++ JavaScript desde cero/1.- Variables/script.js
@@ -0,0 +1,37 @@
+// Variables y tipos de datos
+/* 
+            *Tipos de datos
+Los tipos de datos disponibles en JavaScript son:
+*string: 
+Variable de texto usualmente delimitada por dos comillas
+*number:
+A diferencia de otros lenguajes en JavaScript no existe diferencia entre int y float, ambos pertenecen al tipo de variable number
+*Boolean
+Tipo de dato binario cuyos resultados solo pueden ser true y false
+*/
+
+
+// Para inicializar una variable en JavaScript simplemente utilizaremos los comandos let, var o const dependiendo del caso
+// *var
+// El comando var define una variable global (actualmente poco usado, o usado en ocasiones especificas)
+
+var global1;
+global1 = 'Variable Global';
+
+// *let
+// El comando let define una variable regional
+
+let regional;
+regional = 'variable regional';
+
+// *const
+// El comando const define una variable constante (el valor que se le sea asignado sera el valor permanente) ademas su valor obligatoriamente debera ser inicializado al mismo tiempo de su definicion
+
+// const constante; //Da error
+const constante = 'Variable Constante';
+
+// Se puede definir e inicializar mas de una variable al mismo tiempo
+let variableA = true, variableB = 100, variableC = 'String', variableD;
+
+
+
